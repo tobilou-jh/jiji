70323d9cdd5c70237685d253aa493f5561a72f54
Jaime Hidalgo <samitohb@gmail.com>
2025-05-27 15:57:28 -0500

    Create funcion.h

---
+++ funcion.h
@@ -0,0 +1,5 @@
+void leerComponentes(float puntos[][2]);
+float distancia(float x1, float y1, float x2, float y2);
+void calcularDistancia(float *a, float *b, float *c, float puntos[][2]);
+float perimetro(float *s, float *p, float a, float b, float c);
+float area(float s, float a, float b, float c);
